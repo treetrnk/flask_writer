1aad94da57b518fd0d240138d280888098bf0d04
Nathan Hare <nhare330@gmail.com>
2019-05-04 19:33:03 -0400

    Fix meta tags

--- app/models.py
+++ app/models.py
@@ -141,7 +141,7 @@ class Page(db.Model):
     def description(self):
         if self.summary:
             return self.summary
-        return self.body[0:250] + '...'
+        return self.body[0:247] + '...'
 
     def view_code(self):
         return str(datetime.now().year) + str(datetime.now().isocalendar()[1]) + self.slug
