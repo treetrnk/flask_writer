a2c4f9726af35d70994ffb14b02955531f90ac29
Nathan Hare <nhare330@gmail.com>
2019-05-03 20:26:25 -0400

    Make admin panel more responsive

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -113,10 +113,6 @@ $(document).ready(function() {
 		"lengthMenu": [[10, 25, 50, -1], [10, 25, 50, "All"]]
 	});
 
-	$(function () {
-		$('[data-toggle="tooltip"]').tooltip();
-	})
-
 	if (postType == "chapter" || postType == "post") {
 		readProgress();
 
