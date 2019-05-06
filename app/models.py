b6e563b7d8f6a5f16db961a9101038410db1d1fc
Nathan Hare <nhare330@gmail.com>
2019-05-06 08:51:54 -0400

    Update summary generation

--- app/models.py
+++ app/models.py
@@ -141,7 +141,7 @@ class Page(db.Model):
     def description(self):
         if self.summary:
             return self.summary
-        return self.body[0:247].replace('#','').replace('*','') + '...'
+        return self.body[0:247].replace('#','').replace('*','').replace('--', '&#8212;').replace('_', '') + '...'
 
     def view_code(self):
         return str(datetime.now().year) + str(datetime.now().isocalendar()[1]) + self.slug
