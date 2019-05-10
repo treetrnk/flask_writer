5de66824d6c2681b861579f66b5d2247efb81399
Nathan Hare <nhare330@gmail.com>
2019-05-10 19:14:40 -0400

    Add subscriber model and form

--- app/page/forms.py
+++ app/page/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Email
 
 required = "<span class='text-danger'>*</span>"
 
@@ -8,3 +8,9 @@ class SearchForm(FlaskForm):
     keyword = StringField(f'Username{required}', validators=[DataRequired()])
     submit = SubmitField("<i class='fas fa-search'></i> Search")
 
+class SubscribeForm(FlaskForm):
+    email = StringField(f'Email{required}', validators=[DataRequired(),Email()])
+    first_name = StringField('Name')
+    last_name = StringField('Last Name')
+    subscription = StringField('Subscription')
+    submit = SubmitField('Subscribe')
