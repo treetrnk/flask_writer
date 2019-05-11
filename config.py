ec0436702453d4ed9c8a92bac51b5360d1a15b2d
Nathan Hare <nhare330@gmail.com>
2019-05-10 21:23:24 -0400

    Move db to mariadb

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ datadir = os.path.join(os.path.dirname(basedir), 'data/flask_writer/')
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URI') or \
         'sqlite:///' + os.path.join(datadir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
