f21ce76bcbbcc37e070113f8d77f5f1c3854e2a9
Nathan Hare <nhare330@gmail.com>
2019-08-06 11:23:22 -0400

    Log logins, logouts, and login attempts

--- app/auth/routes.py
+++ app/auth/routes.py
@@ -1,5 +1,5 @@
 from app.auth import bp
-from flask import flash, redirect, render_template, url_for
+from flask import flash, redirect, render_template, url_for, current_app
 from app import db
 from app.models import User
 from app.auth.forms import LoginForm
@@ -14,7 +14,9 @@ def login():
         user = User.query.filter_by(username=form.username.data).first()
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password', 'danger')
+            current_app.logger.warning(f'Permissions Warning: Failed login attempt for user {form.username.data}.\n')
             return redirect(url_for('auth.login'))
+        current_app.logger.info(f'{user.username} logged in.\n')
         login_user(user, remember=form.remember.data)
         flash("You have logged in successfully!", "success")
         return redirect(url_for("admin.pages"))
@@ -22,6 +24,7 @@ def login():
 
 @bp.route('/logout')
 def logout():
+    current_app.logger.info(f'{current_user.username} logged out.\n')
     logout_user()
     flash("You have been logged out.", "success")
     return redirect(url_for('page.home'))
