c55d4dce2dfdd2607ef3be19d4763f8ed840b387
Nathan Hare <nhare330@gmail.com>
2020-01-08 10:47:29 -0500

    Fix today stats

--- app/admin/routes.py
+++ app/admin/routes.py
@@ -550,7 +550,8 @@ def records(day=None):
             Record.date >= (datetime.utcnow() - timedelta(days=365)),
             Record.date <= datetime.utcnow()
         ).first().best
-    stats['today'] = Record.words_by_day(datetime.utcnow())
+    current_app.logger.debug(datetime.utcnow().date())
+    stats['today'] = Record.words_by_day(datetime.utcnow().date())
     while (day <= next_month):
         chart_records += [Record.words_by_day(day)]
         day += timedelta(days=+1)
