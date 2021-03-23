4aaf00bb015583f85ff2cf7b366f2e3c8e74b861
Nathan Hare <nhare330@gmail.com>
2021-03-23 11:44:40 -0400

    Fix forms and required tag asterisk

--- app/admin/forms.py
+++ app/admin/forms.py
@@ -11,17 +11,16 @@ from app.models import Page, User, Tag, Definition, Link, Product
 required = "<span class='text-danger'>*</span>"
 
 class AddUserForm(FlaskForm):
-    username = StringField(f'Username{required}', validators=[DataRequired()])
+    username = StringField(f'Username', validators=[DataRequired()])
     email = StringField('Email', validators=[Email(), Optional()])
     avatar = StringField('Avatar URL', validators=[Length(max=500), Optional()])
     about_me = TextAreaField('About Me')
     timezone = SelectField('Timezone')
-    password = PasswordField(f'Password{required}', validators=[DataRequired()])
-    confirm_password = PasswordField(f'Confirm Password{required}', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField("Add User")
+    password = PasswordField(f'Password', validators=[DataRequired()])
+    confirm_password = PasswordField(f'Confirm Password', validators=[DataRequired(), EqualTo('password')])
 
 class EditUserForm(FlaskForm):
-    username = StringField(f'Username{required}', validators=[DataRequired()])
+    username = StringField(f'Username', validators=[DataRequired()])
     email = StringField('Email', validators=[Email(), Optional()])
     avatar = StringField('Avatar URL', validators=[Length(max=500), Optional()])
     about_me = TextAreaField('About Me')
@@ -29,26 +28,25 @@ class EditUserForm(FlaskForm):
 
     password = PasswordField('Password')
     new_password = PasswordField(f'New Password')
-    confirm_password = PasswordField(f'Confirm Password{required}', validators=[EqualTo('password')])
-    submit = SubmitField("Update User")
+    confirm_password = PasswordField(f'Confirm Password', validators=[EqualTo('password')])
 
 def all_tags():
     return Tag.query.order_by('name')
     
 class AddPageForm(FlaskForm):
-   title = StringField(f'Title{required}', validators=[DataRequired()]) 
-   slug = StringField(f'Slug{required}', validators=[DataRequired()]) 
-   template = SelectField(f'Template{required}', choices=Page.TEMPLATE_CHOICES)
+   title = StringField(f'Title', validators=[DataRequired()]) 
+   slug = StringField(f'Slug', validators=[DataRequired()]) 
+   template = SelectField(f'Template', choices=Page.TEMPLATE_CHOICES)
    parent_id = SelectField('Parent', coerce=int)
    banner = StringField('Banner Image')
    summary = TextAreaField('Summary', validators=[Length(max=300),Optional()])
    author_note = TextAreaField("Author's Note", validators=[Length(max=5000),Optional()], render_kw={'rows':4})
    author_note_location = SelectField('Note Location', coerce=str, choices=Page.AUTHOR_NOTE_LOCATIONS)
    sidebar = TextAreaField('Sidebar', validators=[Length(max=5000), Optional()])
-   body = TextAreaField(f'Body{required}', validators=[DataRequired()])
+   body = TextAreaField(f'Body', validators=[DataRequired()])
    notes = TextAreaField('Notes')
    tags = QuerySelectMultipleField('Tags', query_factory=all_tags, allow_blank=True)
-   user_id = SelectField(f'Author{required}', coerce=int, validators=[DataRequired()])
+   user_id = SelectField(f'Author', coerce=int, validators=[DataRequired()])
    pub_date = DateField('Published Date', validators=[Optional()])
    pub_time = TimeField('Published Date', validators=[Optional()])
    published = BooleanField('Published?')
@@ -75,9 +73,9 @@ class AddTagForm(FlaskForm):
         return True
 
 class DefinitionEditForm(FlaskForm):
-    name = StringField(f'Name{required}', validators=[DataRequired()])
-    type = SelectField(f'Type{required}', validators=[DataRequired()])
-    body = TextAreaField(f'Body{required}', validators=[DataRequired()])
+    name = StringField(f'Name', validators=[DataRequired()])
+    type = SelectField(f'Type', validators=[DataRequired()])
+    body = TextAreaField(f'Body', validators=[DataRequired()])
     hidden_body = TextAreaField("Author's Notes")
     parent_id = SelectField('Parent', coerce=int, render_kw={'data_type': 'select2'})
     tag_id = SelectField(f'Linked Tag', coerce=int, render_kw={'data_type': 'select2'})
@@ -91,32 +89,32 @@ class DefinitionEditForm(FlaskForm):
     #    return True
 
 class SendProductForm(FlaskForm):
-    email = StringField(f'Email{required}', validators=[DataRequired(),Email()])
+    email = StringField(f'Email', validators=[DataRequired(),Email()])
 
 class ProductEditForm(FlaskForm):
-    name = StringField(f'Name{required}', validators=[DataRequired()], render_kw={'id':'page_title_input'})
-    slug = StringField(f'Slug{required}', validators=[DataRequired()], render_kw={'id':'page_slug_input'})
-    price = StringField(f'Price{required}', validators=[DataRequired()])
+    name = StringField(f'Name', validators=[DataRequired()], render_kw={'id':'page_title_input'})
+    slug = StringField(f'Slug', validators=[DataRequired()], render_kw={'id':'page_slug_input'})
+    price = StringField(f'Price', validators=[DataRequired()])
     sale_price = StringField(f'Sale Price')
     description = TextAreaField('Description', validators=[Length(max=1000)])
     image = StringField('Image URL', validators=[Length(max=500)])
     download_path = StringField('Download Path', validators=[Length(max=500)])
     linked_page_id = SelectField('Linked Story', coerce=int, render_kw={'data_type': 'select2'})
-    category_id = SelectField(f'Category{required}', coerce=int, validators=[DataRequired()])
+    category_id = SelectField(f'Category', coerce=int, validators=[DataRequired()])
     sort = IntegerField('Sort #', render_kw={'placeholder':"500"})
     on_sale = BooleanField('On Sale')
     active = BooleanField('Active')
 
 class CategoryEditForm(FlaskForm):
-    name = StringField(f'Name{required}', validators=[DataRequired(),Length(max=150)], render_kw={'id':'page_title_input'})
+    name = StringField(f'Name', validators=[DataRequired(),Length(max=150)], render_kw={'id':'page_title_input'})
     icon = StringField('Category', validators=[Length(max=150)])
     default = BooleanField('Default')
 
 class LinkEditForm(FlaskForm):
-    text = StringField(f'Link Text{required}', validators=[Length(max=1000)])
+    text = StringField(f'Link Text', validators=[Length(max=1000)])
     icon = StringField('Icon', validators=[Length(max=200)])
     format = SelectField('Format', validators=[Length(max=200)])
-    product_id = SelectField(f'Product{required}', coerce=int, validators=[DataRequired()])
+    product_id = SelectField(f'Product', coerce=int, validators=[DataRequired()])
     url = StringField('URL', validators=[Length(max=500)])
     sort = IntegerField('Sort #', render_kw={'placeholder':"500"})
 
@@ -127,23 +125,23 @@ class EmailForm(FlaskForm):
     body = TextAreaField('Body')
 
 class RecordForm(FlaskForm):
-    start_words = IntegerField(f'Start{required}', render_kw={'type': 'number'}, validators=[InputRequired()])
-    end_words = IntegerField(f'End{required}', render_kw={'type': 'number'}, validators=[DataRequired()])
+    start_words = IntegerField(f'Start', render_kw={'type': 'number'}, validators=[InputRequired()])
+    end_words = IntegerField(f'End', render_kw={'type': 'number'}, validators=[DataRequired()])
     overall_words = IntegerField('Story Total', render_kw={'type': 'number'}, validators=[Optional()])
     minutes = IntegerField('Minutes', render_kw={'type': 'number'}, validators=[Optional()])
     comment = StringField('Comment', validators=[Length(max=200)])
     
 class RecordEditForm(FlaskForm):
-    start_words = IntegerField(f'Start{required}', render_kw={'type': 'number'}, validators=[InputRequired()])
-    end_words = IntegerField(f'End{required}', render_kw={'type': 'number'}, validators=[DataRequired()])
+    start_words = IntegerField(f'Start', render_kw={'type': 'number'}, validators=[InputRequired()])
+    end_words = IntegerField(f'End', render_kw={'type': 'number'}, validators=[DataRequired()])
     overall_words = IntegerField('Story Total', render_kw={'type': 'number'}, validators=[Optional()])
     minutes = IntegerField('Minutes', render_kw={'type': 'number'}, validators=[Optional()])
     comment = StringField('Comment', validators=[Length(max=200)])
     date = DateField('Date', render_kw={'type': 'date'})
     
 class FileUploadForm(FlaskForm):
-    file_data = FileField(f'File{required}')
-    folder = SelectField(f'Folder{required}')
+    file_data = FileField(f'File')
+    folder = SelectField(f'Folder')
 
 class DeleteObjForm(FlaskForm):
     obj_id = HiddenField('Object id', validators=[DataRequired()])
--- app/page/forms.py
+++ app/page/forms.py
@@ -3,25 +3,22 @@ from wtforms import StringField, SubmitField, SelectMultipleField, widgets
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import Subscriber
 
-required = "<span class='text-danger'>*</span>"
-
 class MultiCheckboxField(SelectMultipleField):
     widget = widgets.ListWidget(prefix_label=False)
     option_widget = widgets.CheckboxInput()
 
 class SearchForm(FlaskForm):
-    keyword = StringField(f'Username{required}', validators=[DataRequired()])
+    keyword = StringField(f'Username', validators=[DataRequired()])
     submit = SubmitField("<i class='fas fa-search'></i> Search")
 
 class SubscribeForm(FlaskForm):
-    email = StringField(f'Email{required}', validators=[DataRequired(),Email()])
+    email = StringField(f'Email', validators=[DataRequired(),Email()])
     first_name = StringField('Name')
     last_name = StringField('Last Name')
-    subscription = MultiCheckboxField(f'Subscriptions{required}', 
+    subscription = MultiCheckboxField(f'Subscriptions', 
             validators=[DataRequired()], 
             description="Please select one or more.",
         )
-    submit = SubmitField('Subscribe')
 
     def validate_email(self, email):
         subscriber = Subscriber.query.filter_by(email=email.data).first()
@@ -29,7 +26,7 @@ class SubscribeForm(FlaskForm):
             raise ValidationError(f"{email.data} is already subscribed.")
 
 class SubscriptionForm(FlaskForm):
-    subscription = MultiCheckboxField(f'Subscriptions{required}', 
+    subscription = MultiCheckboxField(f'Subscriptions', 
             validators=[DataRequired()], 
             description="Please select one or more.",
         )
